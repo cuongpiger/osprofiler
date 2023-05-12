206e42e1d2f086d6f44e7caa1607fb95df5b1289
cuongpiger <cuongpigerr@gmail.com>
2023-05-12 11:29:14 +0700

    [feat] - allowing devstack install OSprofiler at local

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -5,25 +5,29 @@
 XTRACE=$(set +o | grep xtrace)
 set -o xtrace
 
-source $DEST/osprofiler/devstack/lib/osprofiler
+if [[ "$OSPROFILER_DIRECTORY_PATH" != "" ]]; then
+  source $OSPROFILER_DIRECTORY_PATH/devstack/lib/osprofiler
+else
+  source $DEST/osprofiler/devstack/lib/osprofiler
+fi
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
-    echo_summary "Configuring system services for OSProfiler"
-    install_osprofiler_collector
+  echo_summary "Configuring system services for OSProfiler"
+  install_osprofiler_collector
 
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-    echo_summary "Configuring OSProfiler"
-    configure_osprofiler
+  echo_summary "Configuring OSProfiler"
+  configure_osprofiler
 
 elif [[ "$1" == "stack" && "$2" == "test-config" ]]; then
-    echo_summary "Configuring Tempest"
-    configure_osprofiler_in_tempest
+  echo_summary "Configuring Tempest"
+  configure_osprofiler_in_tempest
 
 elif [[ "$1" == "unstack" ]]; then
-    if [[ "$OSPROFILER_COLLECTOR" == "jaeger" ]]; then
-        echo_summary "Deleting jaeger docker container"
-        drop_jaeger
-    fi
+  if [[ "$OSPROFILER_COLLECTOR" == "jaeger" ]]; then
+    echo_summary "Deleting jaeger docker container"
+    drop_jaeger
+  fi
 fi
 
 # Restore xtrace
